1613eb49d8b6894d297b1aed5e28d67fd980d76f
Maryam-Hashemi <hashemi.maryam86@gmail.com>
2021-09-01 20:23:39 -0600

    "removed extra comments from Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,8 +6,6 @@ export default function Header() {
     <div style={{display:'flex',justifyContent:"space-around", gap:"400px", }}>
       <img className="hi" id="headerImg" src={header} alt="header" width="100%" ></img>
       <img id="logo" src={logo} alt="logo" width="10%" height="10%" ></img>
-      {/* <button type="button">Quiz</button>
-      <button type="button">Assign to</button> */}
     </div>
   )
 }
