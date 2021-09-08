4d4424924f6e2c3ee028b0be7d76adba38fdec82
Maryam-Hashemi <hashemi.maryam86@gmail.com>
2021-09-08 13:35:06 -0600

    pages and cancel buttons

--- src/Components/AddQuestion.js
+++ src/Components/AddQuestion.js
@@ -1,9 +1,11 @@
 import { Link, useHistory } from 'react-router-dom'
+import Header from './Header';
 
 export default function AddQuestion() {
   let history = useHistory();
   return (
     <div>
+      <Header/>
       <Link to='/create/add-question'></Link>
       <button onClick={()=>history.push('/create/quiz/question')}>Add question</button>
     </div>
